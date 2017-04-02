897011c1dabfefb0866773455cf44764adbc8c62
William Norman <lordcrekit@gmail.com>
2017-04-02 16:00:04 -0400

    First working version!

---
+++ assets/search.js
@@ -0,0 +1,18 @@
+var start = 'https://lmgtfy.com?'
+var engines = ['', 's=b&', 's=y&', 's=a&', 's=k&', 's=d&']
+var searchterm = 'q='
+
+var search = function search(e) {
+  var engine = engines[Math.floor(Math.random()*engines.length)]
+  var query = document.getElementById("searchbar").value
+  if (!query || query.trim().length == 0) {
+    window.alert("You must specify a search!")
+    return 
+  } else {
+    query = query.trim()
+  }
+
+  var s = start + engine + searchterm + query 
+
+  window.location.href = s
+}
